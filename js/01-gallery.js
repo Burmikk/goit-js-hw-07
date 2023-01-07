1e10df05a896f70010b3b08950bdd264f44916e3
Burmikk <byrmik@gmail.com>
2023-01-07 17:50:40 +0200

    task -1

---
+++ js/01-gallery.js
@@ -0,0 +1,52 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery")
+
+const galleryMarkup = onCreateGallaryMarkup(galleryItems)
+
+galleryEl.insertAdjacentHTML('beforeend',galleryMarkup)
+
+galleryEl.addEventListener("click", onGetOriginalImage)
+
+
+function onCreateGallaryMarkup(images) {
+    return images.map(({ preview, original, description }) => {
+       return`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+}).join("")       
+}
+
+
+function onGetOriginalImage(event) {
+    event.preventDefault()
+
+    if (!event.target.classList.contains("gallery__image")) {
+       return
+    }
+    const urlOfBigImage = event.target.dataset.source
+    
+    const instance = basicLightbox.create(`
+    <img src="${urlOfBigImage}" width="800" height="600">
+`)
+    instance.show()  
+
+    window.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+             instance.close()
+        }
+    })
+
+}
+
+
+
+
